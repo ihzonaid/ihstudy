18ba4eacdfc3998ab4a00e11d55ef5330129f99f
xementor <ahammedzonaid@gmail.com>
2023-09-19 13:12:34 +0600

    fix Nav link, icon

--- packages/app/components/web/NavBar.tsx
+++ packages/app/components/web/NavBar.tsx
@@ -17,13 +17,13 @@ const Nav = ({ onHamberge }: NavProps) => {
 
   function NavItem({ name }: { name: string }) {
     return (
-      <Link href="/course" className="">
+      <Link href={name == 'Home' ? '/' : '/course'} className="">
         <View
           className=" mr-2 flex  h-10 items-center justify-between
            p-1 hover:border-b-2 hover:border-blue-200 "
         >
           {name == 'Home' ? (
-            <HeroOutline.Bookmark color={'gray'} className="mr-2" />
+            <HeroOutline.Home color={'gray'} className="mr-2" />
           ) : (
             <HeroOutline.BookOpen color={'gray'} className="mr-2" />
           )}
@@ -40,11 +40,13 @@ const Nav = ({ onHamberge }: NavProps) => {
        flex-row items-center  justify-between bg-slate-100 px-4 drop-shadow-md sm:px-16"
       >
         <View className="flex flex-row items-center ">
-          <Pressable className="mr-10 flex h-8 w-20 items-center justify-center rounded-lg bg-blue-400 hover:bg-blue-500 sm:w-36">
-            <Text className="text-center text-base font-bold text-white">
-              ihStudy
-            </Text>
-          </Pressable>
+          <Link href={'/'}>
+            <Pressable className="mr-10 flex h-8 w-20 items-center justify-center rounded-lg bg-blue-400 hover:bg-blue-500 sm:w-36">
+              <Text className="text-center text-base font-bold text-white">
+                ihStudy
+              </Text>
+            </Pressable>
+          </Link>
 
           <View className="flex h-10  flex-row  items-center p-2">
             <NavItem name="Home" />
